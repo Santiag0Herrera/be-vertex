819d40a44806d7e74a4a60f82866c57ffb78379b
Santiago <santi.nico.herrera@gmail.com>
2025-03-25 19:11:58 -0300

    Arreglo de generacion de jwt al auth

--- app/router/auth.py
+++ app/router/auth.py
@@ -46,8 +46,8 @@ class Token(BaseModel):
   token_type: str
 
 
-def authenticate_user(username: str, password: str, db):
-  user = db.query(Users).filter(Users.username == username).first()
+def authenticate_user(email: str, password: str, db):
+  user = db.query(Users).filter(Users.email == email).first()
   if not user:
     return False
   if not bcrypt_context.verify(password, user.hashed_password):
@@ -55,8 +55,8 @@ def authenticate_user(username: str, password: str, db):
   return user
 
 
-def create_token(username: str, user_id: int, role: str, expires_delta: timedelta):
-  encode = {'sub': username, 'id': user_id, 'role': role}
+def create_token(email: str, user_id: int, permission: str, expires_delta: timedelta):
+  encode = {'sub': email, 'id': user_id, 'perm': permission}
   expires = datetime.now(timezone.utc) + expires_delta
   encode.update({'exp': expires})
   return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -65,12 +65,12 @@ def create_token(username: str, user_id: int, role: str, expires_delta: timedelt
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
   try:
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    username: str = payload.get('sub')
+    email: str = payload.get('sub')
     user_id: int = payload.get('id')
-    user_role: str = payload.get('role')
-    if username is None or user_id is None:
+    user_perm: str = payload.get('perm')
+    if email is None or user_id is None:
       raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Credentials') 
-    return {'username': username, 'id': user_id, 'user_role': user_role}
+    return {'email': email, 'id': user_id, 'user_perm': user_perm}
   except JWTError:
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Credentials')
 
@@ -106,7 +106,8 @@ async def get_login_token(form_data: Annotated[OAuth2PasswordRequestForm, Depend
   user = authenticate_user(form_data.username, form_data.password, db)
   if not user: 
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
-  token = create_token(user.username, user.id, user.role, timedelta(minutes=20 ))
+  user_permission = db.query(Permission).filter(user.perm_id == Permission.id).first()
+  token = create_token(user.email, user.id, user_permission.level, timedelta(minutes=20 ))
   return {'access_token': token, 'token_type': 'bearer'}
 
 
