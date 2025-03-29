f5983563623a0000ffbe82f73b30ac40010d8c34
Santiago <santi.nico.herrera@gmail.com>
2025-03-29 11:39:07 -0300

    Conection to db

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test123!@localhost/VertexApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://vertex:vertex2025@cddatabase.cc1ao8osa9zo.us-east-1.rds.amazonaws.com:5432/cddatabase'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
+
+def create_tables():
+    """
+    Creates all tables in the database based on the SQLAlchemy models.
+    """
+    try:
+        print("Creating tables in the database...")
+        Base.metadata.create_all(bind=engine)
+        print("Tables created successfully!")
+    except Exception as e:
+        print(f"An error occurred while creating tables: {e}")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 import models
-from db.database import engine
+from db.database import create_tables
 from router import auth, transactions, users, entities
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+create_tables()
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(transactions.router)
