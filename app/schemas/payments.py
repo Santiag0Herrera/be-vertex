7e488bc06bdfbc8a609783d7ee392b04dd769f57
Santiago <santi.nico.herrera@gmail.com>
2025-11-11 23:31:37 -0300

    little pydantic validation change for new payments

--- app/schemas/payments.py
+++ app/schemas/payments.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel, Field
 
 class NewPaymentRequest(BaseModel):
   amount: float = Field(..., min=1, max=99999999)
-  date: datetime.date = Field(...)
+  date: datetime.datetime = Field(...)
   customer_balance_id: int = Field(...)
   currency_id: int
